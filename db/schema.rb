aef5577258c2a3504b1517045776c16b93cf26e5
ftbyu <ftb.y.u.kashima.07@outlook.com>
2019-09-27 09:26:24 +0000

    あとはbootstrapで綺麗に修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_25_093904) do
+ActiveRecord::Schema.define(version: 2019_09_27_053530) do
 
   create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "body"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +38,5 @@ ActiveRecord::Schema.define(version: 2019_09_25_093904) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
